e53309fccbf30a20cbf7154a91b6d95a5e86585c
Don <serfdon@gmail.com>
2025-11-30 07:34:55 -0800

    Make tools out of rocks

--- init.lua
+++ init.lua
@@ -6,8 +6,8 @@ core.register_node("myrocks:rock", {
 	paramtype = "light",
 	paramtype2 = "facedir",
 	pointable = true,
-	is_ground_content = false,
-	groups = {cracky=3,not_in_creative_inventory=0},
+	is_ground_content = true,
+	groups = {dig_immediate=3,not_in_creative_inventory=1},
 	selection_box = {
 		type = "fixed",
 		fixed = {
@@ -23,8 +23,8 @@ core.register_node("myrocks:rock2", {
 	paramtype = "light",
 	paramtype2 = "facedir",
 	pointable = true,
-	is_ground_content = false,
-	groups = {cracky=3,not_in_creative_inventory=0},
+	is_ground_content = true,
+	groups = {dig_immediate=3,not_in_creative_inventory=1},
 	selection_box = {
 		type = "fixed",
 		fixed = {
@@ -40,12 +40,47 @@ core.register_node("myrocks:rock3", {
 	paramtype = "light",
 	paramtype2 = "facedir",
 	pointable = true,
-	is_ground_content = false,
-	groups = {cracky=3,not_in_creative_inventory=0},
+	is_ground_content = true,
+	groups = {dig_immediate=1,not_in_creative_inventory=1},
 	selection_box = {
 		type = "fixed",
 		fixed = {
 			{-0.1875, -0.5, -0.1875, 0.1875, -0.3125, 0.1875},
+		}
+	}
+})
+core.register_node("myrocks:rock4", {
+	description = "Rock 4",
+	tiles = {"default_stone.png"},
+	drawtype = "mesh",
+	mesh = "myrocks_rock4.obj",
+	paramtype = "light",
+	paramtype2 = "facedir",
+	pointable = true,
+	is_ground_content = true,
+	groups = {dig_immediate=1,not_in_creative_inventory=1},
+	selection_box = {
+		type = "fixed",
+		fixed = {
+			{-0.1875, -0.5, -0.1875, 0.1875, -0.3125, 0.1875},
+		}
+	}
+})
+core.register_node("myrocks:rock5", {
+	description = "Rock 5",
+	tiles = {"default_stone.png"},
+	drawtype = "mesh",
+	mesh = "myrocks_rock5.obj",
+	paramtype = "light",
+	paramtype2 = "facedir",
+	pointable = true,
+	is_ground_content = true,
+	drops = "myrocks:rock 5",
+	groups = {oddly_breakable_by_hand=2,not_in_creative_inventory=1},
+	selection_box = {
+		type = "fixed",
+		fixed = {
+			{-0.5, -0.5, -0.5, 0.5, -0.5, 0.5},
 		}
 	}
 })
@@ -54,7 +89,7 @@ core.register_decoration({
     deco_type = "simple",
     place_on = {"default:dirt_with_grass"},
     sidelen = 16,
-    fill_ratio = 0.002,
+    fill_ratio = 0.0015,
     biomes = {"grassy_plains"},
     y_max = 50,
     y_min = 1,
@@ -64,7 +99,7 @@ core.register_decoration({
     deco_type = "simple",
     place_on = {"default:dirt_with_dry_grass"},
     sidelen = 16,
-    fill_ratio = 0.002,
+    fill_ratio = 0.0015,
     biomes = {"grassy_plains"},
     y_max = 50,
     y_min = 1,
@@ -74,7 +109,7 @@ core.register_decoration({
     deco_type = "simple",
     place_on = {"default:dirt_with_snow"},
     sidelen = 16,
-    fill_ratio = 0.002,
+    fill_ratio = 0.0015,
     biomes = {"grassy_plains"},
     y_max = 50,
     y_min = 1,
@@ -84,7 +119,7 @@ core.register_decoration({
     deco_type = "simple",
     place_on = {"default:desert_stone"},
     sidelen = 16,
-    fill_ratio = 0.002,
+    fill_ratio = 0.0015,
     biomes = {"grassy_plains"},
     y_max = 50,
     y_min = 1,
@@ -94,7 +129,7 @@ core.register_decoration({
     deco_type = "simple",
     place_on = {"default:sand"},
     sidelen = 16,
-    fill_ratio = 0.002,
+    fill_ratio = 0.0015,
     biomes = {"grassy_plains"},
     y_max = 50,
     y_min = 1,
@@ -104,7 +139,7 @@ core.register_decoration({
     deco_type = "simple",
     place_on = {"default:desert_sand"},
     sidelen = 16,
-    fill_ratio = 0.002,
+    fill_ratio = 0.0015,
     biomes = {"grassy_plains"},
     y_max = 50,
     y_min = 1,
@@ -114,7 +149,7 @@ core.register_decoration({
     deco_type = "simple",
     place_on = {"default:silver_sand"},
     sidelen = 16,
-    fill_ratio = 0.002,
+    fill_ratio = 0.0015,
     biomes = {"grassy_plains"},
     y_max = 50,
     y_min = 1,
@@ -127,7 +162,7 @@ core.register_decoration({
     deco_type = "simple",
     place_on = {"default:dirt_with_grass"},
     sidelen = 16,
-    fill_ratio = 0.002,
+    fill_ratio = 0.0015,
     biomes = {"grassy_plains"},
     y_max = 50,
     y_min = 1,
@@ -137,7 +172,7 @@ core.register_decoration({
     deco_type = "simple",
     place_on = {"default:dirt_with_dry_grass"},
     sidelen = 16,
-    fill_ratio = 0.002,
+    fill_ratio = 0.0015,
     biomes = {"grassy_plains"},
     y_max = 50,
     y_min = 1,
@@ -147,7 +182,7 @@ core.register_decoration({
     deco_type = "simple",
     place_on = {"default:dirt_with_snow"},
     sidelen = 16,
-    fill_ratio = 0.002,
+    fill_ratio = 0.0015,
     biomes = {"grassy_plains"},
     y_max = 50,
     y_min = 1,
@@ -157,7 +192,7 @@ core.register_decoration({
     deco_type = "simple",
     place_on = {"default:desert_stone"},
     sidelen = 16,
-    fill_ratio = 0.002,
+    fill_ratio = 0.0015,
     biomes = {"grassy_plains"},
     y_max = 50,
     y_min = 1,
@@ -167,7 +202,7 @@ core.register_decoration({
     deco_type = "simple",
     place_on = {"default:sand"},
     sidelen = 16,
-    fill_ratio = 0.002,
+    fill_ratio = 0.0015,
     biomes = {"grassy_plains"},
     y_max = 50,
     y_min = 1,
@@ -177,7 +212,7 @@ core.register_decoration({
     deco_type = "simple",
     place_on = {"default:desert_sand"},
     sidelen = 16,
-    fill_ratio = 0.002,
+    fill_ratio = 0.0015,
     biomes = {"grassy_plains"},
     y_max = 50,
     y_min = 1,
@@ -187,7 +222,7 @@ core.register_decoration({
     deco_type = "simple",
     place_on = {"default:silver_sand"},
     sidelen = 16,
-    fill_ratio = 0.002,
+    fill_ratio = 0.0015,
     biomes = {"grassy_plains"},
     y_max = 50,
     y_min = 1,
@@ -198,7 +233,7 @@ core.register_decoration({
     deco_type = "simple",
     place_on = {"default:dirt_with_grass"},
     sidelen = 16,
-    fill_ratio = 0.002,
+    fill_ratio = 0.0015,
     biomes = {"grassy_plains"},
     y_max = 50,
     y_min = 1,
@@ -208,7 +243,7 @@ core.register_decoration({
     deco_type = "simple",
     place_on = {"default:dirt_with_dry_grass"},
     sidelen = 16,
-    fill_ratio = 0.002,
+    fill_ratio = 0.0015,
     biomes = {"grassy_plains"},
     y_max = 50,
     y_min = 1,
@@ -218,7 +253,7 @@ core.register_decoration({
     deco_type = "simple",
     place_on = {"default:dirt_with_snow"},
     sidelen = 16,
-    fill_ratio = 0.002,
+    fill_ratio = 0.0015,
     biomes = {"grassy_plains"},
     y_max = 50,
     y_min = 1,
@@ -228,7 +263,7 @@ core.register_decoration({
     deco_type = "simple",
     place_on = {"default:desert_stone"},
     sidelen = 16,
-    fill_ratio = 0.002,
+    fill_ratio = 0.0015,
     biomes = {"grassy_plains"},
     y_max = 50,
     y_min = 1,
@@ -238,7 +273,7 @@ core.register_decoration({
     deco_type = "simple",
     place_on = {"default:sand"},
     sidelen = 16,
-    fill_ratio = 0.002,
+    fill_ratio = 0.0015,
     biomes = {"grassy_plains"},
     y_max = 50,
     y_min = 1,
@@ -248,7 +283,7 @@ core.register_decoration({
     deco_type = "simple",
     place_on = {"default:desert_sand"},
     sidelen = 16,
-    fill_ratio = 0.002,
+    fill_ratio = 0.0015,
     biomes = {"grassy_plains"},
     y_max = 50,
     y_min = 1,
@@ -258,10 +293,317 @@ core.register_decoration({
     deco_type = "simple",
     place_on = {"default:silver_sand"},
     sidelen = 16,
-    fill_ratio = 0.002,
+    fill_ratio = 0.0015,
     biomes = {"grassy_plains"},
     y_max = 50,
     y_min = 1,
     decoration = "myrocks:rock3",
 })
+-----------------------------------------------------
+core.register_decoration({
+    deco_type = "simple",
+    place_on = {"default:dirt_with_grass"},
+    sidelen = 16,
+    fill_ratio = 0.001,
+    biomes = {"grassy_plains"},
+    y_max = 50,
+    y_min = 1,
+    decoration = "myrocks:rock4",
+})
+core.register_decoration({
+    deco_type = "simple",
+    place_on = {"default:dirt_with_dry_grass"},
+    sidelen = 16,
+    fill_ratio = 0.001,
+    biomes = {"grassy_plains"},
+    y_max = 50,
+    y_min = 1,
+    decoration = "myrocks:rock4",
+})
+core.register_decoration({
+    deco_type = "simple",
+    place_on = {"default:dirt_with_snow"},
+    sidelen = 16,
+    fill_ratio = 0.001,
+    biomes = {"grassy_plains"},
+    y_max = 50,
+    y_min = 1,
+    decoration = "myrocks:rock4",
+})
+core.register_decoration({
+    deco_type = "simple",
+    place_on = {"default:desert_stone"},
+    sidelen = 16,
+    fill_ratio = 0.001,
+    biomes = {"grassy_plains"},
+    y_max = 50,
+    y_min = 1,
+    decoration = "myrocks:rock4",
+})
+core.register_decoration({
+    deco_type = "simple",
+    place_on = {"default:sand"},
+    sidelen = 16,
+    fill_ratio = 0.001,
+    biomes = {"grassy_plains"},
+    y_max = 50,
+    y_min = 1,
+    decoration = "myrocks:rock4",
+})
+core.register_decoration({
+    deco_type = "simple",
+    place_on = {"default:desert_sand"},
+    sidelen = 16,
+    fill_ratio = 0.001,
+    biomes = {"grassy_plains"},
+    y_max = 50,
+    y_min = 1,
+    decoration = "myrocks:rock4",
+})
+core.register_decoration({
+    deco_type = "simple",
+    place_on = {"default:silver_sand"},
+    sidelen = 16,
+    fill_ratio = 0.001,
+    biomes = {"grassy_plains"},
+    y_max = 50,
+    y_min = 1,
+    decoration = "myrocks:rock4",
+})
+-----------------------------------------------------
+core.register_decoration({
+    deco_type = "simple",
+    place_on = {"default:dirt_with_grass"},
+    sidelen = 16,
+    fill_ratio = 0.0002,
+    biomes = {"grassy_plains"},
+    y_max = 50,
+    y_min = 1,
+    decoration = "myrocks:rock5",
+})
+core.register_decoration({
+    deco_type = "simple",
+    place_on = {"default:dirt_with_dry_grass"},
+    sidelen = 16,
+    fill_ratio = 0.0002,
+    biomes = {"grassy_plains"},
+    y_max = 50,
+    y_min = 1,
+    decoration = "myrocks:rock5",
+})
+core.register_decoration({
+    deco_type = "simple",
+    place_on = {"default:dirt_with_snow"},
+    sidelen = 16,
+    fill_ratio = 0.0002,
+    biomes = {"grassy_plains"},
+    y_max = 50,
+    y_min = 1,
+    decoration = "myrocks:rock5",
+})
+core.register_decoration({
+    deco_type = "simple",
+    place_on = {"default:desert_stone"},
+    sidelen = 16,
+    fill_ratio = 0.0002,
+    biomes = {"grassy_plains"},
+    y_max = 50,
+    y_min = 1,
+    decoration = "myrocks:rock5",
+})
+core.register_decoration({
+    deco_type = "simple",
+    place_on = {"default:sand"},
+    sidelen = 16,
+    fill_ratio = 0.0002,
+    biomes = {"grassy_plains"},
+    y_max = 50,
+    y_min = 1,
+    decoration = "myrocks:rock5",
+})
+core.register_decoration({
+    deco_type = "simple",
+    place_on = {"default:desert_sand"},
+    sidelen = 16,
+    fill_ratio = 0.0002,
+    biomes = {"grassy_plains"},
+    y_max = 50,
+    y_min = 1,
+    decoration = "myrocks:rock5",
+})
+core.register_decoration({
+    deco_type = "simple",
+    place_on = {"default:silver_sand"},
+    sidelen = 16,
+    fill_ratio = 0.0002,
+    biomes = {"grassy_plains"},
+    y_max = 50,
+    y_min = 1,
+    decoration = "myrocks:rock5",
+})
 
+--Crafts--------------------------------------------------
+minetest.register_craft({
+	output = "default:pick_stone",
+	recipe ={
+			{"myrocks:rock","myrocks:rock","myrocks:rock"},
+			{"","default:stick",""},
+			{"","default:stick",""},
+			}
+})
+minetest.register_craft({
+	output = "default:pick_stone",
+	recipe ={
+			{"myrocks:rock2","myrocks:rock2","myrocks:rock2"},
+			{"","default:stick",""},
+			{"","default:stick",""},
+			}
+})
+minetest.register_craft({
+	output = "default:pick_stone",
+	recipe ={
+			{"myrocks:rock3","myrocks:rock3","myrocks:rock3"},
+			{"","default:stick",""},
+			{"","default:stick",""},
+			}
+})
+minetest.register_craft({
+	output = "default:pick_stone",
+	recipe ={
+			{"myrocks:rock4","myrocks:rock4","myrocks:rock4"},
+			{"","default:stick",""},
+			{"","default:stick",""},
+			}
+})
+
+minetest.register_craft({
+	output = "default:sword_stone",
+	recipe ={
+			{"","myrocks:rock",""},
+			{"","myrocks:rock",""},
+			{"","default:stick",""},
+			}
+})
+minetest.register_craft({
+	output = "default:sword_stone",
+	recipe ={
+			{"","myrocks:rock2",""},
+			{"","myrocks:rock2",""},
+			{"","default:stick",""},
+			}
+})
+minetest.register_craft({
+	output = "default:sword_stone",
+	recipe ={
+			{"","myrocks:rock3",""},
+			{"","myrocks:rock3",""},
+			{"","default:stick",""},
+			}
+})
+minetest.register_craft({
+	output = "default:sword_stone",
+	recipe ={
+			{"","myrocks:rock4",""},
+			{"","myrocks:rock4",""},
+			{"","default:stick",""},
+			}
+})
+
+minetest.register_craft({
+	output = "default:shovel_stone",
+	recipe ={
+			{"","myrocks:rock",""},
+			{"","default:stick",""},
+			{"","default:stick",""},
+			}
+})
+minetest.register_craft({
+	output = "default:shovel_stone",
+	recipe ={
+			{"","myrocks:rock2",""},
+			{"","default:stick",""},
+			{"","default:stick",""},
+			}
+})
+minetest.register_craft({
+	output = "default:shovel_stone",
+	recipe ={
+			{"","myrocks:rock3",""},
+			{"","default:stick",""},
+			{"","default:stick",""},
+			}
+})
+minetest.register_craft({
+	output = "default:shovel_stone",
+	recipe ={
+			{"","myrocks:rock4",""},
+			{"","default:stick",""},
+			{"","default:stick",""},
+			}
+})
+
+minetest.register_craft({
+	output = "default:axe_stone",
+	recipe ={
+			{"myrocks:rock","myrocks:rock",""},
+			{"myrocks:rock","default:stick",""},
+			{"","default:stick",""},
+			}
+})
+minetest.register_craft({
+	output = "default:axe_stone",
+	recipe ={
+			{"myrocks:rock","myrocks:rock2",""},
+			{"myrocks:rock","default:stick",""},
+			{"","default:stick",""},
+			}
+})
+minetest.register_craft({
+	output = "default:axe_stone",
+	recipe ={
+			{"myrocks:rock","myrocks:rock3",""},
+			{"myrocks:rock","default:stick",""},
+			{"","default:stick",""},
+			}
+})
+minetest.register_craft({
+	output = "default:axe_stone",
+	recipe ={
+			{"myrocks:rock","myrocks:rock4",""},
+			{"myrocks:rock","default:stick",""},
+			{"","default:stick",""},
+			}
+})
+
+minetest.register_craft({
+	output = "farming:hoe_stone",
+	recipe ={
+			{"myrocks:rock","myrocks:rock",""},
+			{"","default:stick",""},
+			{"","default:stick",""},
+			}
+})
+minetest.register_craft({
+	output = "farming:hoe_stone",
+	recipe ={
+			{"myrocks:rock","myrocks:rock2",""},
+			{"","default:stick",""},
+			{"","default:stick",""},
+			}
+})
+minetest.register_craft({
+	output = "farming:hoe_stone",
+	recipe ={
+			{"myrocks:rock","myrocks:rock3",""},
+			{"","default:stick",""},
+			{"","default:stick",""},
+			}
+})
+minetest.register_craft({
+	output = "farming:hoe_stone",
+	recipe ={
+			{"myrocks:rock","myrocks:rock4",""},
+			{"","default:stick",""},
+			{"","default:stick",""},
+			}
+})
